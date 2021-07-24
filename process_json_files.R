d2321bfae0d8aa4e7803f1744f57ee34b1793b04
Jessica Nicosia <j.nicosia@wustl.edu>
2021-07-23 19:11:27 -0700

    processing script and testthat updates
    - updated elements of processing script so output csvs look more like the ultimate desired outcome
    - updated testthat script to work with novel data
    - neither the processing script nor testthat work with the legacy data at this point

--- process_json_files.R
+++ process_json_files.R
@@ -9,19 +9,23 @@
 #
 #
 
+
 library(tidyverse)
 library(readit)
 library(tidyjson)
 library(R2HTML)
 library(jsonlite)
 
+#rm(list = ls(all = TRUE))
+#files <- list.files("../test_data/HASD_assessment_data_20-07-21_copy", ".*\\.json", full.names = TRUE)
+
 process_json_files<-function(files) {
-	n_files<-length(files)
+	n_files <- length(files)
 	
 	output_files <- list()
 
-	pricesout <- matrix(, nrow = 1, ncol = 7)
-	colnames(pricesout) <- c("subid","session","prices_date","pricesCorrect","pricesSelected", "Trial", "device")
+	pricesout <- matrix(nrow = 1, ncol = 7)
+	colnames(pricesout) <- c("subid", "session", "prices_date", "pricesCorrect","pricesSelected", "Trial", "device")
 	
 	for (i in 1:n_files){
 	  file <- files[i]
@@ -45,29 +49,39 @@ process_json_files<-function(files) {
 	  pricesdf <-as.data.frame(pricesdf) # convert to data frame
 	  pricesdf$Trial <- seq.int(nrow(pricesdf))
 	  
-	  colnames(pricesdf) = c("subid","session","prices_date","pricesCorrect","pricesSelected",  "device", "Trial")
+	  colnames(pricesdf) = c("subid", "session", "prices_date", "pricesCorrect", "pricesSelected", "device", "Trial")
 	  
 	  pricesout <- rbind(pricesout,pricesdf)
 	}
 	
 	pricesout$acc <- ifelse(pricesout$pricesCorrect == pricesout$pricesSelected, 1,0)
+	
 	# drop first row
 	pricesout <- pricesout[-1,]
+	
 	# convert session to numeric
 	pricesout$session <- as.numeric(pricesout$session)
-	# sort by session
-	pricesout <- arrange(pricesout, session)
 	
 	# remove duplicate observations
-	
 	pricesout <- pricesout %>% distinct(subid, session,Trial, .keep_all = TRUE) %>% ungroup()
 	
 	pricesout <- pricesout %>% group_by(subid,session, prices_date, device) %>% summarise(prices = mean(acc)) %>% ungroup()
 	
 	pricesout$prices_err <- 100*(1-pricesout$prices)
-	sage_prices<-file.path(tempdir(), "sage_prices.csv")
-	write.csv(pricesout,sage_prices,row.names = F )
-	output_files$sage_prices<-sage_prices
+	
+	# convert unix date to date format
+	pricesout$prices_date <- as.Date(as.POSIXct(as.numeric(pricesout$prices_date), origin = "1970-01-01")) 
+	
+	# sort by subid and session
+	pricesout <- dplyr::arrange(pricesout, subid, session) 
+	
+	# reorder and select the columns we want
+	pricesout <- subset(pricesout, select = c(subid, device, session, prices_date, prices, prices_err))
+	
+	# output
+	sage_prices <- file.path(tempdir(), "sage_prices.csv")
+	write.csv(pricesout, sage_prices, row.names = F )
+	output_files$sage_prices <- sage_prices
 	
 	
 	#========================================================================
@@ -159,19 +173,19 @@ process_json_files<-function(files) {
 	
 	
 	# number of trials
-	numSections=2
+	numSections = 2
 	
 	# number of items per trial
-	numItems=3
+	numItems = 3
 	
 	# size of the screen grid
-	GridSizeX=5
-	GridSizeY=5
+	GridSizeX = 5
+	GridSizeY = 5
 	
 	# merge grids all together
 	
-	gridsout <- matrix(, nrow = 1, ncol = 17)
-	colnames(gridsout) <- c("tapx.1","tapx.2","tapx.3","tapy.1","tapy.2","tapy.3","imagex.1","imagex.2","imagex.3","imagey.1","imagey.2","imagey.3","Trial","session","subid", "grids_date", "grids")
+	gridsout <- matrix(nrow = 1, ncol = 18)
+	colnames(gridsout) <- c("tapx.1", "tapx.2", "tapx.3", "tapy.1", "tapy.2", "tapy.3", "imagex.1", "imagex.2", "imagex.3", "imagey.1", "imagey.2", "imagey.3", "Trial", "session", "subid", "grids_date", "grids", "device")
 	
 	for (i in 1:n_files){
 	  file = files[i]
@@ -180,16 +194,19 @@ process_json_files<-function(files) {
 	  
 	  subid <- foo$participant_id
 	  session <- foo$session_id
-	   grids_date = foo$session_date
+	  grids_date <- foo$session_date
+	  device <- foo$device_info
 	
 	  foo <- as.data.frame(foo$tests$grid_test$sections)
-	  check <- foo$display_symbols
-	  check <- as.data.frame(check)
-	  len_check <- length(check)
-	  if(len_check == 0) next # if blank skip to next iteration
 	  
-	  len_check2 = ifelse(foo$display_symbols < 0, 0,1)
-	  if(len_check2 == 0) next # if blank skip to next iteration
+	  #check <- foo$display_symbols
+	  #check <- as.data.frame(check)
+	  #len_check <- length(check)
+	  #if(len_check == 0) next # if blank skip to next iteration
+	  #len_check2 = ifelse(foo$display_symbols < 0, 0, 1)
+	  #if(len_check2 == 0) next # if blank skip to next iteration
+	  if(nrow(foo) == 0) next # if there aren't any rows (trials), then go to next iteration (file) # NEEDS TO BE CONFIRMED
+	  
 	  gridstap <- foo$choices
 	  gridsTap1 <- as.data.frame(gridstap[1])
 	  gridsTap2 <- as.data.frame(gridstap[2])
@@ -217,34 +234,53 @@ process_json_files<-function(files) {
 	  gridsimagesx.1 <- gridsimages1[1,"x"]
 	  gridsimagesx.2 <- gridsimages1[2,"x"]  
 	  gridsimagesx.3 <-  gridsimages1[3,"x"]   
-	  gridsimagesx.4 <-gridsimages2[1,"x"]
+	  gridsimagesx.4 <- gridsimages2[1,"x"]
 	  gridsimagesx.5 <-  gridsimages2[2,"x"] 
 	  gridsimagesx.6 <-  gridsimages2[3,"x"] 
 	  
-	  gridsimagesy.1 <-gridsimages1[1,"y"]
+	  gridsimagesy.1 <- gridsimages1[1,"y"]
 	  gridsimagesy.2 <- gridsimages1[2,"y"]  
 	  gridsimagesy.3 <-  gridsimages1[3,"y"]   
-	  gridsimagesy.4 <-gridsimages2[1,"y"]
+	  gridsimagesy.4 <- gridsimages2[1,"y"]
 	  gridsimagesy.5 <-  gridsimages2[2,"y"] 
 	  gridsimagesy.6 <-  gridsimages2[3,"y"] 
 	  
 	  # make a matrix of those items. one row per trial 
-	  table1 <- matrix(c(gridsTapx.1,gridsTapx.2,gridsTapx.3,
-	                     gridsTapy.1,gridsTapy.2,gridsTapy.3,
-	                     gridsimagesx.1,gridsimagesx.2,gridsimagesx.3,
-	                     gridsimagesy.1,gridsimagesy.2,gridsimagesy.3,
-	                     gridsTapx.4,gridsTapx.5,gridsTapx.6,
-	                     gridsTapy.4,gridsTapy.5,gridsTapy.6,
-	                     gridsimagesx.4,gridsimagesx.5,gridsimagesx.6,
-	                     gridsimagesy.4,gridsimagesy.5,gridsimagesy.6),
+	  
+	  matrix_input_check <- c(gridsTapx.1, gridsTapx.2, gridsTapx.3,
+                            gridsTapy.1, gridsTapy.2, gridsTapy.3,
+                            gridsimagesx.1, gridsimagesx.2, gridsimagesx.3,
+                            gridsimagesy.1, gridsimagesy.2, gridsimagesy.3,
+                            gridsTapx.4, gridsTapx.5, gridsTapx.6,
+                            gridsTapy.4, gridsTapy.5, gridsTapy.6,
+                            gridsimagesx.4, gridsimagesx.5, gridsimagesx.6,
+                            gridsimagesy.4, gridsimagesy.5, gridsimagesy.6)
+	  
+	  if (length(matrix_input_check) != 24){
+	    warning(paste(file, "has missing choices for grids."))
+	  }
+	 
+	  table1 <- matrix(c(gridsTapx.1, gridsTapx.2, gridsTapx.3,
+	                     gridsTapy.1, gridsTapy.2, gridsTapy.3,
+	                     gridsimagesx.1, gridsimagesx.2, gridsimagesx.3,
+	                     gridsimagesy.1, gridsimagesy.2, gridsimagesy.3,
+	                     gridsTapx.4, gridsTapx.5, gridsTapx.6,
+	                     gridsTapy.4, gridsTapy.5, gridsTapy.6,
+	                     gridsimagesx.4, gridsimagesx.5, gridsimagesx.6,
+	                     gridsimagesy.4, gridsimagesy.5, gridsimagesy.6),
 	                   nrow = 2, ncol = 12, byrow = TRUE)
+	  
+	  
+	  
 	  table1 <- as.data.frame(table1)
-	  colnames(table1) <- c("tapx.1", "tapx.2","tapx.3","tapy.1", "tapy.2","tapy.3","imagex.1", "imagex.2","imagex.3","imagey.1", "imagey.2","imagey.3")
+	  colnames(table1) <- c("tapx.1", "tapx.2", "tapx.3", "tapy.1", "tapy.2", "tapy.3", "imagex.1", "imagex.2", "imagex.3", "imagey.1", "imagey.2", "imagey.3")
 	  
 	  table1$Trial <- rownames(table1) 
 	  table1$session <- session
 	  table1$subid <- subid
 	  table1$grids_date <- grids_date
+	  table1$device <- device
+	  
 	  # convert each to numeric
 	  table1$imagex.1 <- as.numeric(table1$imagex.1)
 	  table1$imagex.2 <- as.numeric(table1$imagex.2)
@@ -266,27 +302,38 @@ process_json_files<-function(files) {
 	  choices.y = table1 %>% select(tapy.1, tapy.2, tapy.3)
 	  
 	  
-	  tak_grids = grid_test_computeDistance (numSections, numItems,images.x, images.y, choices.x, choices.y)
+	  tak_grids = grid_test_computeDistance (numSections, numItems, images.x, images.y, choices.x, choices.y)
 	  table1$grids <- tak_grids
 	  
 	  
-	  gridsout <- rbind(gridsout,table1)
+	  gridsout <- rbind(gridsout, table1)
 	}
 	
 	# this has a duplication for each trial so can just subset to Trial 1
 	gridsout <- gridsout %>% distinct(subid, session, .keep_all = TRUE) %>% ungroup()
+	
 	# drop first row
 	gridsout <- gridsout[-1,]
+	
+	# convert unix date to date format
+	gridsout$grids_date <- as.Date(as.POSIXct(as.numeric(gridsout$grids_date), origin = "1970-01-01")) 
+	
+	# sort by subid and session
+	gridsout <- dplyr::arrange(gridsout, subid, as.numeric(session))
+	
+	# reorder and select the columns we want (Trial, tapx.1, tapx.2, tapx.3, tapy.1, tapy.2, tapy.3, imagex.1, imagex.2, imagex.3, imagey.1, imagey.2, imagey.3 may be removed later)
+	gridsout <- subset(gridsout, select = c(subid, device, session, grids_date, grids))
+	
 	sage_grids <- file.path(tempdir(), "sage_grids.csv")
 	write.csv(gridsout, sage_grids, row.names= F)
-	output_files$sage_grids<-sage_grids
+	output_files$sage_grids <- sage_grids
 	
 	# Processing symbols data -------------------------------------------------
 	
 	# merge symbols all together
 	
-	symbolsout <- matrix(, nrow = 1, ncol = 8)
-	colnames(symbolsout) <- c("subid","session","symbols_date", "symbolsCorrect","symbolsSelected", "selectionTime", "appearanceTime", "Trial")
+	symbolsout <- matrix(nrow = 1, ncol = 9)
+	colnames(symbolsout) <- c("subid", "session", "symbols_date", "symbolsCorrect","symbolsSelected", "selectionTime", "appearanceTime", "device", "Trial")
 	
 	for (i in 1:n_files){
 	  file = files[i]
@@ -295,6 +342,7 @@ process_json_files<-function(files) {
 	  
 	  subid <- foo$participant_id
 	  session <- foo$session_id
+	  device <- foo$device_info
 	  
 	  symbols_date = foo$session_date
 	  
@@ -307,35 +355,48 @@ process_json_files<-function(files) {
 	  selectionTime <- foo$selection_time
 	  appearanceTime <- foo$appearance_time
 	  
-	  symbolsdf <- cbind(subid, session, symbols_date, symbolsCorrect, symbolsSelected, selectionTime,appearanceTime)
-	  symbolsdf <-as.data.frame(symbolsdf) # convert to data frame
+	  symbolsdf <- cbind(subid, session, symbols_date, symbolsCorrect, symbolsSelected, selectionTime, appearanceTime, device)
+	  symbolsdf <- as.data.frame(symbolsdf) # convert to data frame
 	  
-	  colnames(symbolsdf) = c("subid","session","symbols_date", "symbolsCorrect","symbolsSelected", "selectionTime", "appearanceTime")
+	  colnames(symbolsdf) = c("subid", "session", "symbols_date", "symbolsCorrect","symbolsSelected", "selectionTime", "appearanceTime", "device")
 	  
 	  #add trial number
 	  symbolsdf$Trial <- seq.int(nrow(symbolsdf))
-	  symbolsout <- rbind(symbolsout,symbolsdf)
+	  symbolsout <- rbind(symbolsout, symbolsdf)
 	}
+	
 	# drop first row
 	symbolsout <- symbolsout[-1,]
+	
 	# define accuracy
 	symbolsout$acc <- ifelse(symbolsout$symbolsCorrect == symbolsout$symbolsSelected, 1, 0)
+	
 	#define RT
 	symbolsout$RT <- as.numeric(symbolsout$selectionTime) - as.numeric(symbolsout$appearanceTime)
 	
 	# convert session to numberic
 	symbolsout$session <- as.numeric(symbolsout$session)
-	# sort by session
-	symbolsout <- arrange(symbolsout, session)
 	
 	# remove duplicate observations
 	symbolsout <- symbolsout %>% distinct(subid, session,Trial, .keep_all = TRUE) %>% ungroup()
 	
 	# write the data to a .csv
-	
-	symbolsout <- symbolsout %>% group_by(subid,session,symbols_date) %>% filter(acc == 1) %>% summarise(symbols = median(RT), symbols_acc = (n()/12)) %>% ungroup()
+	symbolsout <- symbolsout %>% 
+	  group_by(subid, device, session, symbols_date) %>% 
+	  filter(acc == 1) %>% 
+	  summarise(symbols = median(RT), symbols_acc = (n()/12)) %>% 
+	  ungroup()
 
-	sage_symbols<-file.path(tempdir(), "sage_symbols.csv")
+	# convert unix date to date format
+	symbolsout$symbols_date <- as.Date(as.POSIXct(as.numeric(symbolsout$symbols_date), origin = "1970-01-01")) 
+	
+	# sort by subid and session
+	symbolsout <- dplyr::arrange(symbolsout, subid, session) 
+	
+	# reorder and select the columns we want
+	symbolsout <- subset(symbolsout, select = c(subid, device, session, symbols_date, symbols, symbols_acc))
+
+	sage_symbols <- file.path(tempdir(), "sage_symbols.csv")
 	write.csv(symbolsout, sage_symbols,row.names = F )
 	output_files$sage_symbols <- sage_symbols
 	
@@ -348,49 +409,58 @@ process_json_files<-function(files) {
 	
 	
 	
-	data <- left_join(p, g, by = c("subid","session"))
-	data <- left_join(data, s, by = c("subid","session"))
+	data <- left_join(p, g, by = c("subid", "device", "session"))
+	data <- left_join(data, s, by = c("subid", "device", "session"))
 	
 	# code for visit if timestamps are more than 7 months apart
-	data = data %>% arrange(subid, prices_date)
+	data <- data %>% arrange(subid, prices_date)
 	
-	data$date = as.POSIXct(data$prices_date, origin = "1970-01-01")
+	data$date <- as.POSIXct(data$prices_date, origin = "1970-01-01")
 	
-	data=data %>% mutate(date_diff = date - lag(date)) 
-	
-	data = data %>% group_by(subid) %>% mutate(visit = case_when(
-	  session <= 27 ~ 1,
-	  session <= 55 ~ 2,
-	  session <= 83 ~ 3,
-	  session <= 111 ~ 4,
-	  session <= 139 ~ 5,
-	  session <= 167 ~ 6,
-	  session <= 195 ~ 7,
-	  session <= 223 ~ 8,
-	))
+	data <- data %>% mutate(date_diff = date - lag(date)) 
+
+# what happens if someone has low adherence? would visit # be off at that point? this should be fixed	
+	data <- data %>% group_by(subid) %>% 
+	  mutate(visit = case_when(
+	    session <= 27 ~ 1,
+	    session <= 55 ~ 2,
+	    session <= 83 ~ 3,
+	    session <= 111 ~ 4,
+	    session <= 139 ~ 5,
+	    session <= 167 ~ 6,
+	    session <= 195 ~ 7,
+	    session <= 223 ~ 8))
 	
 	# keep only the variables I need in the final dataframe
-	data <- data %>% select(subid, session,prices_date, symbols_date, grids_date, prices_err,grids, symbols, symbols_acc, visit)
+	data <- data %>% select(subid, device, session, visit, prices_date, symbols_date, grids_date, prices_err, grids, symbols, symbols_acc)
 	
 	
 	# drop session 0 as it is just training
-	if (FALSE) {
+#	if (FALSE) {
 		# The next line has a bug:  'hasd' is read before it's defined
-		hasd <- subset(hasd, session >= 1)
-		
+		hasd <- subset(data, session >= 1)
 		
 		get_num_sesh <-  hasd %>% 
-		  group_by(subid) %>% 
-		  summarise( num_sesh = n(), bl_date = min(date))  %>% ungroup() %>% select(subid, num_sesh, bl_date)
+		  group_by(subid, visit) %>% 
+		  summarise(num_sesh = n(), bl_date = min(prices_date)) %>% 
+		  ungroup() %>% 
+		  select(subid, visit, num_sesh, bl_date)
 		
-		hasd <- left_join(hasd, get_num_sesh, by = "subid")
+		hasd <- full_join(hasd, get_num_sesh)
 		
-		hasd = hasd %>% group_by(subid, visit) %>% summarize(Prices = mean(prices_err, na.rm = T), Symbols = mean(symbols, na.rm = T), Grids = mean(grids, na.rm = T), arc_date = min(date, na.rm = T), number_sessions_completed = n()) %>% mutate(adherence = number_sessions_completed / 28)
+		hasd <- hasd %>% 
+		  group_by(subid, session, visit) %>% 
+		  summarize(Prices = mean(prices_err, na.rm = T), 
+		            Symbols = mean(symbols, na.rm = T), 
+		            Grids = mean(grids, na.rm = T), 
+		            arc_date = min(prices_date, na.rm = T),
+		            num_sesh = unique(num_sesh)) %>% 
+		  mutate(adherence = num_sesh / 28)
 		
-		hasd_merged<-file.path(tempdir(), "hasd_merged.csv")
-		write.csv(hasd, file = hasd_merged,row.names = F)
-		output_files$hasd_merged<-hasd_merged
-	}
+		hasd_merged <-  file.path(tempdir(), "hasd_merged.csv")
+		write.csv(hasd, file = hasd_merged, row.names = F)
+		output_files$hasd_merged <- hasd_merged
+#	}
 	
 	output_files
 }
--- tests/testthat/test_process_novel_json_files.R
+++ tests/testthat/test_process_novel_json_files.R
@@ -3,32 +3,32 @@ context("test process_json_files using novel data")
 source("../../process_json_files.R")
 
 library(readr)
+library(testthat)
 
 test_that("test process_json_files using novel data", {
-  json_files<-list.files("../test_data/novel", ".*\\.json", full.names=TRUE)
+  json_files <- list.files("../test_data/novel", ".*\\.json", full.names = TRUE)
   expect_gt(length(json_files), 1)
   
   # function under test
   output_files <- process_json_files(json_files)
 
-  expect_equal(length(output_files), 3)
+  print(output_files)
   
+  expect_equal(length(output_files), 4)
   
   expect_true(file.exists(output_files$sage_symbols))
-  #expected_sage_symbols<-data.frame(
-  #	subid=c(440001, 440001, 600600, 600622), 
-  #	session=c(0,33,0,0),
-  #	symbols_date=c(1615478953.52758, 1632159012, 1618586613.616, 1623347426.727),
-  #	symbols=c(0.13780701160431, 0.307, 0.206000000000001, 3.366),
-  #	symbols_acc=c(0.583333333333333, 0.416666666666667, 0.25,0.916666666666667)
-  #	)
-  #	expect_equal(read.csv(output_files$sage_symbols), expected_sage_symbols)
-  	
+  expected_sage_symbols <- read.csv("../expected_result/sage_symbols.csv")	
+  expect_equal(read.csv(output_files$sage_symbols), expected_sage_symbols)
+
   expect_true(file.exists(output_files$sage_grids))
-  # TODO compare expected to actual 'sage_grids'
-  	
+  expected_sage_grids <- read.csv("../expected_result/sage_grids.csv")	
+  expect_equal(read.csv(output_files$sage_grids), expected_sage_grids)
+
   expect_true(file.exists(output_files$sage_prices))
-  # TODO compare expected to actual 'sage_prices'
-  	
-  # TODO compare expected to actual 'hasd_merged'
+  expected_sage_prices <- read.csv("../expected_result/sage_prices.csv")	
+  expect_equal(read.csv(output_files$sage_prices), expected_sage_prices)
+
+  expect_true(file.exists(output_files$hasd_merged))
+  expected_hasd_merged <- read.csv("../expected_result/hasd_merged.csv")	
+  expect_equal(read.csv(output_files$hasd_merged), expected_hasd_merged)
 })
